20492dec9f8009ae3f2d6e9945446325081df87e
Yuriy Vaskin <vaskin90@gmail.com>
2016-11-10 22:58:48 +0300

    - added alleles option

--- README.md
+++ README.md
@@ -1,9 +1,9 @@
 # gaconverter
 Converts GeneticAnalyzer genotyping .rtf files to xlsx format
 
-# How to run:
+# How to run the converter:
 ```
-python converter.py [PATH_TO_RTF] [PATH_TO_XLSX]
+python converter.py convert [PATH_TO_RTF] [PATH_TO_XLSX]
 ```
 Converted file will be created in the XLSX folder with modified name
 
--- converter.py
+++ converter.py
@@ -2,6 +2,14 @@ import click
 
 from gaconverter.converter import Converter
 
+@click.group()
+def cli():
+    pass
+
+
+@click.command()
+def alleles():
+    click.echo('Dropped the database')
 
 @click.command()
 @click.argument('rtf_path', nargs=1)
@@ -10,5 +18,8 @@ def convert(rtf_path, xlsx_path):
     converter = Converter(rtf_path, xlsx_path)
     converter.run()
 
+cli.add_command(convert)
+cli.add_command(alleles)
+
 if __name__ == '__main__':
-    convert()
+    cli()
